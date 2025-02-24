cced7220d8b4391c6dba7790ab8bbc995d189d8c
Tomasz Kosobucki <kosobucki.tomek@gmail.com>
2025-02-24 17:02:13 +0100

    initial project setup using spring initializr

---
+++ src/main/java/com/tkoso/de_lingo_boost/DeLingoBoostApplication.java
@@ -0,0 +1,13 @@
+package com.tkoso.de_lingo_boost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeLingoBoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeLingoBoostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkoso/de_lingo_boost/DeLingoBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkoso.de_lingo_boost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeLingoBoostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
